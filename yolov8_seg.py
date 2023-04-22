383c17259b6ccfacea5c5944d0a1fc2e63a8e590
Emerald-Z <ejzhang2004@gmail.com>
2023-04-22 09:48:23 -0500

    seg

--- yolov8_seg.py
+++ yolov8_seg.py
@@ -4,7 +4,7 @@ from yolo_segmentation import YOLOSegmentation
 import numpy as np
 import torch
 
-cap = cv2.VideoCapture("move_cups.mp4")
+cap = cv2.VideoCapture(0)
 
 # load model
 model = YOLOSegmentation("yolov8n-seg.pt") # medium coco dataset
@@ -12,27 +12,29 @@ model = YOLOSegmentation("yolov8n-seg.pt") # medium coco dataset
 def segment_image(frame):
     W, H, _ = frame.shape
     # pass frame to model
-    bboxes, classes, segmentations, scores, masks = model.detect(frame) #use model.detect form yolov8m
+    try:
+        bboxes, classes, segmentations, scores, masks = model.detect(frame) #use model.detect form yolov8m
+    except:
+        return frame
     for bbox, class_id, seg, score, mask in zip(bboxes, classes, segmentations, scores, masks):
-        (x, y, x2, y2) = bbox
-        # if  class_id == 32: show only one thing
-        cv2.rectangle(frame, (x, y), (x2, y2), (0, 0, 255), 2)
+        if  class_id == 41: # show only one thing
+            (x, y, x2, y2) = bbox
+            # cv2.rectangle(frame, (x, y), (x2, y2), (0, 0, 255), 2)
 
-        # polygonal edges
-        cv2.polylines(frame, [seg], True, (255, 0, 0), 2) # True - closed
-        cv2.fillPoly(frame, [seg], (255, 0, 0)) # change transparency
+            # polygonal edges
+        
+            cv2.putText(frame, str(class_id), (x, y - 10), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255))
 
-        cv2.putText(frame, str(class_id), (x, y - 10), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255))
+            mask = np.zeros(frame.shape[:2], dtype="uint8")
+            # cv2.polylines(mask, [seg], True, (255, 0, 0), 2) # True - closed
+            cv2.fillPoly(mask, [seg], (255, 255, 255)) # change transparency
 
-        # mask = mask * 255
-        # mask = cv2.resize(mask, (W, H))
-        # cv2.imwrite("img", mask)
 
-        # redImg = np.zeros(frame.shape, frame.dtype)
-        # redImg[:,:] = (0, 0, 255)
-        # redMask = cv2.bitwise_and(redImg, redImg, mask=mask)
-        # cv2.addWeighted(redMask, 1, frame, 1, 0, frame)
-        return frame # segmented image
+            masked_img = cv2.bitwise_and(frame, frame, mask=mask)
+            # cv2.addWeighted(redMask, 1, frame, 1, 0, frame)
+            return masked_img # segmented image
+        else: 
+            return frame
     
 while True: # allows you to click through frames
     ret, frame = cap.read()
